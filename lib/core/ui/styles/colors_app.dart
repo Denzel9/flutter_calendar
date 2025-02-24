785bd9bb9699e14ed1eb70bede033bb3d4b73d3a
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2025-02-24 11:26:42 +0300

    PR to code review of source repository denzel9/flutter_calendar

--- lib/core/ui/styles/colors_app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorsApp {
-  static Color get primary => const Color.fromRGBO(247, 200, 14, 1);
-  static Color get secondary => Colors.white;
-  static Color get bg => const Color.fromRGBO(40, 40, 40, 1);
-  static Color get dark => const Color.fromARGB(255, 24, 24, 24);
-}
--- lib/service/auth/auth_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract interface class AuthService {
-  bool isLoading = false;
-  Future<User> register(String login, String password);
-  Future<User> login(String login, String password);
-}
